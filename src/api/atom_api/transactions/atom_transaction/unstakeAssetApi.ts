01fd6006e86c153e048682f98ff6beab88b6998d
wmcashe <wmcashe@gmail.com>
2024-03-21 19:54:08 +0800

    :shit: 修复 unstake api 找不到的 bug

--- src/api/atom_api/transactions/atom_transaction/unstakeAssetApi.ts
+++ src/api/atom_api/transactions/atom_transaction/unstakeAssetApi.ts
@@ -2,7 +2,7 @@ import { TransactionApi } from "./_transactionApi";
 import { GENERATE_TRANSACTION_API_PATH } from "../../../../constants";
 
 export class UnstakeAssetApi extends TransactionApi {
-    readonly GENERATE_API_PATH = GENERATE_TRANSACTION_API_PATH.TR_TRUST_ASSET;
+    readonly GENERATE_API_PATH = GENERATE_TRANSACTION_API_PATH.TR_UNSTAKE_ASSET;
     readonly CREATE_API_PATH = "trUnstakeAsset";
     readonly PACKAGE_API_PATH = "trUnstakeAssetWithSign";
     readonly BROADCAST_API_PATH = "send/unstakeAsset";
